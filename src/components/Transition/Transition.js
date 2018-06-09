f2a5c9d36476fb8cd4b75259cf321e71148127ce
Godsenal <tmqps78@gmail.com>
2018-06-09 18:00:52 +0900

    Update make Transition component to stateless

--- src/components/Transition/Transition.js
+++ src/components/Transition/Transition.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import CSSTransition from 'react-transition-group/CSSTransition';
 
 import './transition.css';
 
-export default class Transition extends Component {
-  static propTypes = {
-    animation: PropTypes.string,
-    children: PropTypes.node.isRequired,
-    in: PropTypes.bool.isRequired,
-    timeout: PropTypes.number,
-  }
-  static defaultProps = {
-    animation: 'boom',
-    timeout: 300,
-  }
-  render() {
-    const { animation, children } = this.props;
-    return (
-      <CSSTransition classNames={animation} {...this.props} unmountOnExit>
-        {React.cloneElement(children)}
-      </CSSTransition>
-    );
-  }
-}
+const Transition = ({ animation, children, ...props }) => (
+  <CSSTransition classNames={animation} {...props} unmountOnExit>
+    {React.cloneElement(children)}
+  </CSSTransition>
+);
+
+Transition.propTypes = {
+  animation: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  in: PropTypes.bool.isRequired,
+  timeout: PropTypes.number,
+};
+Transition.defaultProps = {
+  animation: 'boom',
+  timeout: 300,
+};
+
+export default Transition;
