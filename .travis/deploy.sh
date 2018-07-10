42ab8ec13538ea786de918f2a2fac08c3ee91f43
Godsenal <tmqps78@gmail.com>
2018-07-10 22:22:30 +0900

    Update travis remote name

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,5 +3,5 @@ set -e
 git config --global push.default simple # we only want to push one branch — master
 # specify the repo on the live server as a remote repo, and name it 'production'
 # <user> here is the separate user you created for deploying
-git remote add production ssh://tmqps78@45.76.98.255/home/tmqps78/LTH_React/devlog
-git push production master # push our updates
+git remote add deploy ssh://tmqps78@45.76.98.255/home/tmqps78/LTH_React/devlog
+git push deploy master # push our updates
