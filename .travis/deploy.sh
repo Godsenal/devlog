9b0869998d37bbb3116dd9f6d842d6681517644e
Godsenal <tmqps78@gmail.com>
2018-07-11 17:31:21 +0900

    Update travis environment variable

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,7 +3,7 @@ set -e
 git config --global push.default simple # we only want to push one branch — master
 # specify the repo on the live server as a remote repo, and name it 'production'
 # <user> here is the separate user you created for deploying
-git remote add deploy ssh://tmqps78@$IP$DEPLOY_DIR # travis environment varialbe setting
+git remote add deploy ssh://tmqps78@$IP$DEPLOY_DIR # travis environment varialbe
 git push deploy master # push our updates
 
 ssh apps@$IP <<EOF
