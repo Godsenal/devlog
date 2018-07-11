33b76c682a8bc8057e765598154df06457de4e85
Godsenal <tmqps78@gmail.com>
2018-07-11 21:08:48 +0900

    Update uglify setting

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -32,6 +32,13 @@ module.exports = ({
     runtimeChunk: true,
     minimizer: [
       new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          compress: false,
+          ecma: 6,
+          mangle: true,
+        },
         sourceMap: true,
       }),
     ],
